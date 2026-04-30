821be771429135163704ede47acf95be5ba82095
bin <17426470+boyan01@users.noreply.github.com>
2026-04-30 11:20:11 +0800

    [data detector] chore: update objective_c dependencies

--- packages/data_detector/lib/data_detector.dart
+++ packages/data_detector/lib/data_detector.dart
@@ -1,7 +1,7 @@
 import 'dart:ffi';
+import 'dart:typed_data';
 import 'dart:ui';
 
-import 'package:ffi/ffi.dart';
 import 'package:objective_c/objective_c.dart' as objc;
 
 import 'src/data_detector_bindings_generated.dart' as binding;
@@ -10,10 +10,12 @@ export 'src/data_detector_bindings_generated.dart'
     show NSTextCheckingType, NSMatchingOptions;
 
 extension on TextRange {
-  Pointer<binding.NSRange> toNSRange() {
-    final range = malloc.allocate<binding.NSRange>(sizeOf<binding.NSRange>());
-    range.ref.location = start;
-    range.ref.length = end - start;
+  objc.NSRange toNSRange() {
+    final range = Struct.create<objc.NSRange>(
+      Uint8List(sizeOf<objc.NSRange>()),
+    );
+    range.location = start;
+    range.length = end - start;
     return range;
   }
 }
@@ -23,21 +25,10 @@ class TextCheckingResult {
 
   final binding.NSTextCheckingResult _inner;
 
-  binding.NSTextCheckingType get type => _inner.resultType;
+  int get type => _inner.resultType;
 
   TextRange get range {
-    final msgSend = objc.msgSendPointer
-        .cast<
-            NativeFunction<
-                binding.NSRange Function(
-                    Pointer<objc.ObjCObject>, Pointer<objc.ObjCSelector>)>>()
-        .asFunction<
-            binding.NSRange Function(
-                Pointer<objc.ObjCObject>, Pointer<objc.ObjCSelector>)>();
-    final property = objc.registerName("range");
-
-    // the generated _inner.getRange() not working on macos x64, call send manually
-    final range = msgSend(_inner.pointer, property);
+    final range = _inner.range;
     final textRange =
         TextRange(start: range.location, end: range.location + range.length);
     return textRange;
@@ -64,15 +55,8 @@ class TextCheckingResult {
 }
 
 class DataDetector {
-  factory DataDetector(binding.NSTextCheckingType type) {
-    final error = malloc
-        .allocate<Pointer<objc.ObjCObject>>(sizeOf<Pointer<objc.ObjCObject>>());
-    final detector =
-        binding.NSDataDetector.alloc().initWithTypes_error_(type.value, error);
-    if (error.value != nullptr) {
-      final err = objc.NSError.castFromPointer(error.value);
-      throw err;
-    }
+  factory DataDetector(int type) {
+    final detector = binding.NSDataDetector.alloc().initWithTypes(type);
     return DataDetector._(detector!);
   }
 
@@ -82,20 +66,18 @@ class DataDetector {
 
   List<TextCheckingResult> matchesInString(
     String str, {
-    binding.NSMatchingOptions? options,
+    int? options,
     TextRange? range,
   }) {
     final nsRange = (range ?? TextRange(start: 0, end: str.length)).toNSRange();
-    final array = _detector.matchesInString_options_range_(
-        str.toNSString(),
-        options ?? binding.NSMatchingOptions.NSMatchingReportCompletion,
-        nsRange.ref);
-    malloc.free(nsRange);
+    final array = _detector.matchesInString(str.toNSString(),
+        options:
+            options ?? binding.NSMatchingOptions.NSMatchingReportCompletion,
+        range: nsRange);
 
     final results = <TextCheckingResult>[];
     for (var i = 0; i < array.count; i++) {
-      final result =
-          binding.NSTextCheckingResult.castFrom(array.objectAtIndex_(i));
+      final result = binding.NSTextCheckingResult.as(array.objectAtIndex(i));
       results.add(TextCheckingResult._(result));
     }
     return results;
--- packages/data_detector/lib/src/data_detector_bindings_generated.dart.m
+++ packages/data_detector/lib/src/data_detector_bindings_generated.dart.m
@@ -1,11 +1,117 @@
 #include <stdint.h>
-
+#import <Foundation/Foundation.h>
+#import <objc/message.h>
 #import <Foundation/NSRegularExpression.h>
-typedef void  (^ListenerBlock)(NSTextCheckingResult* , NSMatchingFlags , BOOL * );
-ListenerBlock wrapListenerBlock_ObjCBlock_ffiVoid_NSTextCheckingResult_NSMatchingFlags_bool(ListenerBlock block) {
-  ListenerBlock wrapper = [^void(NSTextCheckingResult* arg0, NSMatchingFlags arg1, BOOL * arg2) {
-    block([arg0 retain], arg1, arg2);
-  } copy];
-  [block release];
-  return wrapper;
+
+#if !__has_feature(objc_arc)
+#error "This file must be compiled with ARC enabled"
+#endif
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wundeclared-selector"
+
+typedef struct {
+  int64_t version;
+  void* (*newWaiter)(void);
+  void (*awaitWaiter)(void*);
+  void* (*currentIsolate)(void);
+  void (*enterIsolate)(void*);
+  void (*exitIsolate)(void);
+  int64_t (*getMainPortId)(void);
+  bool (*getCurrentThreadOwnsIsolate)(int64_t);
+} DOBJC_Context;
+
+id objc_retainBlock(id);
+
+#define BLOCKING_BLOCK_IMPL(ctx, BLOCK_SIG, INVOKE_DIRECT, INVOKE_LISTENER)    \
+  assert(ctx->version >= 1);                                                   \
+  void* targetIsolate = ctx->currentIsolate();                                 \
+  int64_t targetPort = ctx->getMainPortId == NULL ? 0 : ctx->getMainPortId();  \
+  return BLOCK_SIG {                                                           \
+    void* currentIsolate = ctx->currentIsolate();                              \
+    bool mayEnterIsolate =                                                     \
+        currentIsolate == NULL &&                                              \
+        ctx->getCurrentThreadOwnsIsolate != NULL &&                            \
+        ctx->getCurrentThreadOwnsIsolate(targetPort);                          \
+    if (currentIsolate == targetIsolate || mayEnterIsolate) {                  \
+      if (mayEnterIsolate) {                                                   \
+        ctx->enterIsolate(targetIsolate);                                      \
+      }                                                                        \
+      INVOKE_DIRECT;                                                           \
+      if (mayEnterIsolate) {                                                   \
+        ctx->exitIsolate();                                                    \
+      }                                                                        \
+    } else {                                                                   \
+      void* waiter = ctx->newWaiter();                                         \
+      INVOKE_LISTENER;                                                         \
+      ctx->awaitWaiter(waiter);                                                \
+    }                                                                          \
+  };
+
+
+typedef BOOL  (^_ProtocolTrampoline)(void * sel);
+__attribute__((visibility("default"))) __attribute__((used))
+BOOL  _DataDetectorBindings_protocolTrampoline_e3qsqz(id target, void * sel) {
+  return ((_ProtocolTrampoline)((id (*)(id, SEL, SEL))objc_msgSend)(target, @selector(getDOBJCDartProtocolMethodForSelector:), sel))(sel);
 }
+
+typedef void  (^_ListenerTrampoline)(void * arg0, id arg1);
+__attribute__((visibility("default"))) __attribute__((used))
+_ListenerTrampoline _DataDetectorBindings_wrapListenerBlock_18v1jvf(_ListenerTrampoline block) NS_RETURNS_RETAINED {
+  return ^void(void * arg0, id arg1) {
+    objc_retainBlock(block);
+    block(arg0, (__bridge id)(__bridge_retained void*)(arg1));
+  };
+}
+
+typedef void  (^_BlockingTrampoline)(void * waiter, void * arg0, id arg1);
+__attribute__((visibility("default"))) __attribute__((used))
+_ListenerTrampoline _DataDetectorBindings_wrapBlockingBlock_18v1jvf(
+    _BlockingTrampoline block, _BlockingTrampoline listenerBlock,
+    DOBJC_Context* ctx) NS_RETURNS_RETAINED {
+  BLOCKING_BLOCK_IMPL(ctx, ^void(void * arg0, id arg1), {
+    objc_retainBlock(block);
+    block(nil, arg0, (__bridge id)(__bridge_retained void*)(arg1));
+  }, {
+    objc_retainBlock(listenerBlock);
+    listenerBlock(waiter, arg0, (__bridge id)(__bridge_retained void*)(arg1));
+  });
+}
+
+typedef void  (^_ProtocolTrampoline_1)(void * sel, id arg1);
+__attribute__((visibility("default"))) __attribute__((used))
+void  _DataDetectorBindings_protocolTrampoline_18v1jvf(id target, void * sel, id arg1) {
+  return ((_ProtocolTrampoline_1)((id (*)(id, SEL, SEL))objc_msgSend)(target, @selector(getDOBJCDartProtocolMethodForSelector:), sel))(sel, arg1);
+}
+
+typedef id  (^_ProtocolTrampoline_2)(void * sel, id arg1);
+__attribute__((visibility("default"))) __attribute__((used))
+id  _DataDetectorBindings_protocolTrampoline_xr62hr(id target, void * sel, id arg1) {
+  return ((_ProtocolTrampoline_2)((id (*)(id, SEL, SEL))objc_msgSend)(target, @selector(getDOBJCDartProtocolMethodForSelector:), sel))(sel, arg1);
+}
+
+typedef void  (^_ListenerTrampoline_1)(id arg0, NSMatchingFlags arg1, BOOL * arg2);
+__attribute__((visibility("default"))) __attribute__((used))
+_ListenerTrampoline_1 _DataDetectorBindings_wrapListenerBlock_6jvo9y(_ListenerTrampoline_1 block) NS_RETURNS_RETAINED {
+  return ^void(id arg0, NSMatchingFlags arg1, BOOL * arg2) {
+    objc_retainBlock(block);
+    block((__bridge id)(__bridge_retained void*)(arg0), arg1, arg2);
+  };
+}
+
+typedef void  (^_BlockingTrampoline_1)(void * waiter, id arg0, NSMatchingFlags arg1, BOOL * arg2);
+__attribute__((visibility("default"))) __attribute__((used))
+_ListenerTrampoline_1 _DataDetectorBindings_wrapBlockingBlock_6jvo9y(
+    _BlockingTrampoline_1 block, _BlockingTrampoline_1 listenerBlock,
+    DOBJC_Context* ctx) NS_RETURNS_RETAINED {
+  BLOCKING_BLOCK_IMPL(ctx, ^void(id arg0, NSMatchingFlags arg1, BOOL * arg2), {
+    objc_retainBlock(block);
+    block(nil, (__bridge id)(__bridge_retained void*)(arg0), arg1, arg2);
+  }, {
+    objc_retainBlock(listenerBlock);
+    listenerBlock(waiter, (__bridge id)(__bridge_retained void*)(arg0), arg1, arg2);
+  });
+}
+#undef BLOCKING_BLOCK_IMPL
+
+#pragma clang diagnostic pop
